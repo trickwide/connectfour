638c8931fb900be18e752f908e0185f9563e2e74
trickwide <82200433+trickwide@users.noreply.github.com>
2024-01-26 22:46:18 +0200

    Board-luokan päivitys

--- src/board.py
+++ src/board.py
@@ -4,8 +4,6 @@ Connect Four Board Module
 
 import numpy as np
 
-ROW_COUNT = 6
-COLUMN_COUNT = 7
 
 class Board:
     """
@@ -16,6 +14,8 @@ class Board:
         """
         Initialize a Connect Four board.
         """
+        self.row_count = 6
+        self.column_count = 7
         self.board = self.generate_board()
 
     def generate_board(self):
@@ -25,19 +25,39 @@ class Board:
         Returns:
             array: A 2D array representing the Connect Four board.
         """
-        board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+        board = np.zeros((self.row_count, self.column_count))
         return board
 
-    def drop_chip(self, row, column, chip):
+    def copy(self):
         """
-        Drop a chip on the board at the specified row and column.
+        Create a copy of the current board.
+
+        Returns:
+            Board: A copy of the current board.
+        """
+        board_copy = Board()
+        board_copy.board = self.board.copy()
+        return board_copy
+
+    def drop_chip(self, column, chip):
+        """
+        Drop a chip into the specified column of the board.
 
         Args:
-            row (int): The row where the chip will be placed.
             column (int): The column where the chip will be placed.
             chip (int): The player's chip (1 for red, 2 for yellow)
+
+        Returns:
+            tuple: A tuple representing the location of the dropped chip (row, column).
+            Otherwise, None.
         """
-        self.board[row][column] = chip
+        row_counter = self.row_count - 1
+        for _ in range(self.row_count):
+            if self.board[row_counter][column] == 0:
+                self.board[row_counter][column] = chip
+                return (row_counter, column)
+            row_counter -= 1
+        return None
 
     def is_valid_location(self, column):
         """
@@ -49,7 +69,7 @@ class Board:
         Returns:
             bool: True if the column is valid, False otherwise.
         """
-        return self.board[ROW_COUNT-1][column] == 0
+        return self.board[0][column] == 0
 
     def get_next_empty_row(self, column):
         """
@@ -61,8 +81,74 @@ class Board:
         Returns:
             int: The row index of the next available empty row in the column.
         """
-        for r in range(ROW_COUNT):
-            if self.board[r][column] == 0:
-                return r
+        for row in range(self.row_count-1, -1, -1):
+            if self.board[row][column] == 0:
+                return row
 
         return -1
+
+    def is_winner(self, player_id):
+        """
+        Check if a given player has won the game.
+
+        Args:
+            player_id (int): The id of the player (1 for red, 2 for yellow).
+
+        Returns:
+           bool: True if the player has won, False otherwise.
+        """
+        # Check for a horizontal win
+        for row in range(self.row_count):
+            for column in range(self.column_count - 3):
+                if self.board[row][column] == player_id and \
+                        self.board[row][column+1] == player_id and \
+                        self.board[row][column+2] == player_id and \
+                        self.board[row][column+3] == player_id:
+                    return True
+
+        # Check for a vertical win
+        for column in range(self.column_count):
+            for row in range(self.row_count - 3):
+                if self.board[row][column] == player_id and \
+                        self.board[row+1][column] == player_id and \
+                        self.board[row+2][column] == player_id and \
+                        self.board[row+3][column] == player_id:
+                    return True
+
+        # Check for a diagonal win (positive slope)
+        for row in range(self.row_count - 3):
+            for column in range(self.column_count - 3):
+                if self.board[row][column] == player_id and \
+                        self.board[row+1][column+1] == player_id and \
+                        self.board[row+2][column+2] == player_id and \
+                        self.board[row+3][column+3] == player_id:
+                    return True
+
+        # Check for a diagonal win (negative slope)
+        for row in range(3, self.row_count):
+            for column in range(self.column_count - 3):
+                if self.board[row][column] == player_id and \
+                        self.board[row-1][column+1] == player_id and \
+                        self.board[row-2][column+2] == player_id and \
+                        self.board[row-3][column+3] == player_id:
+                    return True
+
+        return False
+
+    def is_game_over(self):
+        """
+        Check if the game is over.
+
+        Returns:
+            bool: True if the game is over, False otherwise.
+        """
+        return self.is_board_full() or self.is_winner(1) or self.is_winner(2)
+
+    def is_board_full(self):
+        """
+        Check if the board is full.
+
+        Returns:
+            bool: True if the board is full, False otherwise.
+        """
+        return np.all(self.board != 0)
