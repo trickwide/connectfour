b489af966a66ae19abbc0b159da1f2ca0893eed2
trickwide <82200433+trickwide@users.noreply.github.com>
2024-02-02 19:56:51 +0200

    Player-luokan testien lisäys

---
+++ src/tests/test_player.py
@@ -0,0 +1,29 @@
+import unittest
+from src.player import Player
+
+
+class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player1 = Player(1)
+        self.player2 = Player(2)
+
+    def test_get_id(self):
+        """Test if the correct player ID is returned."""
+        self.assertEqual(self.player1.get_id(), 1,
+                         "Should return 1 for player 1")
+        self.assertEqual(self.player2.get_id(), 2,
+                         "Should return 2 for player 2")
+
+    def test_get_color(self):
+        """Test if the correct RGB color code is returned for each player."""
+        self.assertEqual(self.player1.get_color(), (255, 0, 0),
+                         "Should return red for player 1")
+        self.assertEqual(self.player2.get_color(), (255, 255, 0),
+                         "Should return yellow for player 2")
+
+    def test_get_name(self):
+        """Test if the correct name is returned for each player."""
+        self.assertEqual(self.player1.get_name(), "Player (Red)",
+                         "Should return 'Player (Red)' for player 1")
+        self.assertEqual(self.player2.get_name(), "AI (Yellow)",
+                         "Should return 'AI (Yellow)' for player 2")
